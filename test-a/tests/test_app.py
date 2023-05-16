ff827646ed1918773d130b0356aca7f9cfd09593
Sungan-Kang <sungan.kang@wesang.com>
2023-05-16 17:36:45 +0900

    12 commit

--- test-a/tests/test_app.py
+++ test-a/tests/test_app.py
@@ -5,6 +5,6 @@ from chalice.test import Client
 
 def test_index_route():
     with Client(app.app) as client:
-        response = client.http.get('/')
+        response = client.http.get("/")
         assert response.status_code == 200
-        assert response.json_body == {'hello': 'test-a'}
+        assert response.json_body == {"hello": "test-a"}
